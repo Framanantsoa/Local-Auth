1fd6435b473627bee7d4b30ed2050b9a3a697e89
Framanantsoa <153351321+Framanantsoa@users.noreply.github.com>
2025-11-20 19:13:33 +0300

    Fixing unique email validation

--- backend/DTO/InfoUpdateDto.cs
+++ backend/DTO/InfoUpdateDto.cs
@@ -4,6 +4,10 @@ namespace DTO;
 
 public class InfoUpdateDto : UserInfoDto
 {
+    [Required(ErrorMessage = "L'email est obligatoire.")]
+    [EmailAddress(ErrorMessage = "L'addresse e-mail est incorrect.")]
+    public string email {get; set;}
+
     public DateOnly? naissance {get; set;}
     public string nouveauMotDePasse { get; set; }
 }
--- backend/DTO/RegisterDto.cs
+++ backend/DTO/RegisterDto.cs
@@ -6,4 +6,9 @@ namespace DTO;
 public class RegisterDto : UserInfoDto
 {
     public DateOnly? naissance {get; set;}
+
+    [Required(ErrorMessage = "L'email est obligatoire.")]
+    [EmailAddress(ErrorMessage = "L'addresse e-mail est incorrect.")]
+    [UniqueEmail]
+    public string email {get; set;}
 }
